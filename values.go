ca62e6fc8017a6ec2eac7314375db229007a0896
l0ksh <lokesh904@gmail.com>
2023-06-25 23:58:37 +0530

    Initial commit

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt" 	//Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+
+func main() {
+	
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	
+}
---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b,c int = 2, 7
+	fmt.Println(b,c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "go is awesome"
+	fmt.Println(f)
+
+	g := 27
+	fmt.Println(g)
+}
